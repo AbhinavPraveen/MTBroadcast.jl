f76b167be16b90190fd08cf8a98af06d6807ea88
AbhinavPraveen <50986710+AbhinavPraveen@users.noreply.github.com>
2020-03-22 09:26:41 +0000

    Update docstring to reflect argument cleanup

--- src/mtBroadcast.jl
+++ src/mtBroadcast.jl
@@ -3,7 +3,7 @@ module mtBroadcast
 export mtB,mtBcall
 
 """
-    mtB(f,x...[; n = Threads.nthreads(), t = Any])
+    mtB(f,x...[; n = Threads.nthreads()])
 
 Applies the function 'f' to the varargs 'x...' using 'n' threads and returns the output. 
 """
@@ -24,7 +24,7 @@ function mtB(f, x...; n = Threads.nthreads())
 end
 
 """
-    mtBcall(f,x...[; n = Threads.nthreads(), t = Any])
+    mtBcall(f,x...[; n = Threads.nthreads()])
 
 Assigns 'n' threads to apply the function 'f' to the varargs 'x...' and returns the generated task objects. You may wait.() or fetch.() these objects. This is significantly faster than mtB() if the task objects can be handled efficiently.
 """
